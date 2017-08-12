1e7c6c7cdcb13194e8c8917a8b0fb07dcfbcd3cd
nezlayaya <nezlayaya94@mail.ru>
2017-08-12 14:54:38 +0300

    responsive

--- js/custom.js
+++ js/custom.js
@@ -1,82 +1,85 @@
 /*----------------------------------Slider--------------------------*/
 
-		var currentSlide = 0;
-		var sliderForRemoving = 0;
-		var slider = document.querySelectorAll('.item');
-		var listLi = document.querySelectorAll('li');
-		var ul = document.querySelector('ul');
-		var interval = null;
+var currentSlide = 0;
+var sliderForRemoving = 0;
+var slider = document.querySelectorAll('.item');
+var listLi = document.querySelectorAll('li');
+var ul = document.querySelector('ul');
+var interval = null;
 
-		ul.addEventListener('click', switchCurrentSlide);
-		document.addEventListener('keyup', onKeyup);
-		
-		playInterval();
+ul.addEventListener('click', switchCurrentSlide);
+document.addEventListener('keyup', onKeyup);
 
-		function playInterval() {
-			if (interval) clearInterval(interval);
+playInterval();
 
-			interval = setInterval(function() {
-				clickRight();
-			}, 5000)
-		}
+function playInterval() {
+    if (interval) clearInterval(interval);
 
-		function onKeyup(e) {
-			if(e.key==='ArrowLeft')
-		        {
-		            clickLeft();
-		        }
-		        if(e.key==='ArrowRight')
-		        {
-		            clickRight();
-		    	}
-		}
+    interval = setInterval(function () {
+        clickRight();
+    }, 5000)
+}
 
-		function switchCurrentSlide(e) {
-			if (e.target.value || e.target.value === 0) {
-				sliderForRemoving = currentSlide;
-				currentSlide = e.target.value;
-				changeSlide();
-			}
-		}
+function onKeyup(e) {
+    if (e.key === 'ArrowLeft') {
+        clickLeft();
+    }
+    if (e.key === 'ArrowRight') {
+        clickRight();
+    }
+}
 
-		function clickLeft() {
-			if (currentSlide === 0) {
-				sliderForRemoving = 0;
-				currentSlide = slider.length -1;
-			} else {
-				sliderForRemoving = currentSlide;
-				currentSlide --;
-			}
-			changeSlide();
-		}
-		function clickRight() {
-			if (currentSlide === slider.length -1) {
-				sliderForRemoving = slider.length -1;
-				currentSlide = 0;
-			} else {
-				sliderForRemoving = currentSlide;
-				currentSlide ++;
-			}
-			changeSlide();
-		}
+function switchCurrentSlide(e) {
+    if (e.target.value || e.target.value === 0) {
+        sliderForRemoving = currentSlide;
+        currentSlide = e.target.value;
+        changeSlide();
+    }
+}
 
-		function changeSlide() {
-			slider[sliderForRemoving].classList.remove('show');
-			slider[currentSlide].classList.add('show');
+function clickLeft() {
+    if (currentSlide === 0) {
+        sliderForRemoving = 0;
+        currentSlide = slider.length - 1;
+    } else {
+        sliderForRemoving = currentSlide;
+        currentSlide--;
+    }
+    changeSlide();
+}
 
-			listLi[sliderForRemoving].classList.remove('active');
-			listLi[currentSlide].classList.add('active');
-		}
+function clickRight() {
+    if (currentSlide === slider.length - 1) {
+        sliderForRemoving = slider.length - 1;
+        currentSlide = 0;
+    } else {
+        sliderForRemoving = currentSlide;
+        currentSlide++;
+    }
+    changeSlide();
+}
+
+function changeSlide() {
+    slider[sliderForRemoving].classList.remove('show');
+    slider[currentSlide].classList.add('show');
+
+    listLi[sliderForRemoving].classList.remove('active');
+    listLi[currentSlide].classList.add('active');
+}
 
 /*----------------------Slow scroll----------------------------------------*/
 
 
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+$(document).ready(function () {
+    $(".menu").on("click", "a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $(".menu-icon").on("click", function () {
+        $("header .menu").slideToggle();
     });
 });
 
